a97e03bfe56926f79070190e6c6407ab5752aca8
teogorghin <teogorghin@gmail.com>
2016-09-07 11:29:24 +0300

    Completed TODO task

--- src/main/java/Downer.java
+++ src/main/java/Downer.java
@@ -8,14 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Downer extends JavaPlugin {
     @Override
     public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
+        Boolean pluginenabled = true;
         System.out.println("Downer started...");
         getServer().getPluginManager().registerEvents(new EventListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+        Boolean pluginenabled = false;
     }
 
 }
