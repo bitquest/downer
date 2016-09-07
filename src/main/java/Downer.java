12f018774e00d45f1a59ff541ce8799353757c6c
teogorghin <teogorghin@gmail.com>
2016-09-07 11:30:57 +0300

    Bug fix

--- src/main/java/Downer.java
+++ src/main/java/Downer.java
@@ -6,16 +6,19 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by cristian on 1/29/16.
  */
 public class Downer extends JavaPlugin {
+    
+    public Boolean pluginenabled;
+    
     @Override
     public void onEnable() {
-        Boolean pluginenabled = true;
+        pluginenabled = true;
         System.out.println("Downer started...");
         getServer().getPluginManager().registerEvents(new EventListener(), this);
     }
 
     @Override
     public void onDisable() {
-        Boolean pluginenabled = false;
+        pluginenabled = false;
     }
 
 }
